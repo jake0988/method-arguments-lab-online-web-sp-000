c0059100d3acb999601c29d027cbd34c8541981a
Jake G <jake098877@gmail.com>
2020-04-25 19:56:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def meth(arg)
+def introduction(arg)
   puts "Hi, my name is #{arg}.\n"
 end
 
-def sec_meth(arg, val)
+def introduction_with_language(arg, val)
   puts "Hi, my name is #{arg} and I am learning to program in #{val}./n"
 end
